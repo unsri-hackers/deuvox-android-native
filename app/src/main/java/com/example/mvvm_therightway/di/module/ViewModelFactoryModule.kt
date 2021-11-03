75b9ce9429bdb1ede12ec524dcedaae571f33b6b
ichsanachmad <achmad.ichsan@dana.id>
2021-11-03 22:58:06 +0700

    add singleton on view model factory

--- app/src/main/java/com/example/mvvm_therightway/di/module/ViewModelFactoryModule.kt
+++ app/src/main/java/com/example/mvvm_therightway/di/module/ViewModelFactoryModule.kt
@@ -4,9 +4,11 @@ import androidx.lifecycle.ViewModelProvider
 import com.example.mvvm_therightway.base.viewmodel.ViewModelFactory
 import dagger.Binds
 import dagger.Module
+import javax.inject.Singleton
 
 @Module
 abstract class ViewModelFactoryModule {
     @Binds
+    @Singleton
     abstract fun bindsViewModelFactory(viewModelFactory: ViewModelFactory): ViewModelProvider.Factory
 }
