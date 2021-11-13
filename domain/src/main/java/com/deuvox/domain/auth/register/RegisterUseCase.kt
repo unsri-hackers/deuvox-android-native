9d214c2d342d0df2ea5b3c182847b6b66b7f7fea
Ichvandi <ichvandi.maulana@stockbit.com>
2021-11-13 22:07:50 +0700

    Wrap return type with Resource

--- domain/src/main/java/com/deuvox/domain/auth/register/RegisterUseCase.kt
+++ domain/src/main/java/com/deuvox/domain/auth/register/RegisterUseCase.kt
@@ -3,6 +3,7 @@ package com.deuvox.domain.auth.register
 import com.deuvox.domain.auth.AuthRepository
 import com.deuvox.domain.auth.model.Auth
 import com.deuvox.domain.base.BaseUseCase
+import com.deuvox.domain.base.Resource
 import io.reactivex.rxjava3.core.Observable
 import javax.inject.Inject
 
@@ -10,7 +11,7 @@ class RegisterUseCase @Inject constructor(
     private val authRepository: AuthRepository
 ) : BaseUseCase<Auth, RegisterParam>() {
 
-    override fun buildUseCase(params: RegisterParam): Observable<Auth> {
+    override fun buildUseCase(params: RegisterParam): Observable<Resource<Auth>> {
         return authRepository.register(params)
     }
 
