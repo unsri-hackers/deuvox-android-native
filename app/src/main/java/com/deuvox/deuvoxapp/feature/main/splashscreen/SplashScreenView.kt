2b3995f7a11442f7947c72e72d00ce27732e92fc
onirutlA <alturino001@gmail.com>
2021-11-13 16:31:16 +0700

    add: splash screen view

---
+++ app/src/main/java/com/deuvox/deuvoxapp/feature/main/splashscreen/SplashScreenView.kt
@@ -0,0 +1,23 @@
+package com.deuvox.deuvoxapp.feature.main.splashscreen
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import com.deuvox.deuvoxapp.base.view.BaseRichView
+import com.deuvox.deuvoxapp.databinding.SplashScreenBinding
+
+class SplashScreenView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : BaseRichView<SplashScreenBinding>(context, attrs, defStyleAttr) {
+
+    override fun getViewBinding(): SplashScreenBinding =
+        SplashScreenBinding.inflate(LayoutInflater.from(context), this, true)
+
+    override fun setup() {
+        binding.run {
+
+        }
+    }
+}
