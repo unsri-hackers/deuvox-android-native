dcb5e3c2493c96a7345f2648f2ac102ba3639000
Ichvandi <ichvandi.maulana@stockbit.com>
2021-11-13 22:02:36 +0700

    Add mapper for RegisterParam to RegisterRequest

--- data/src/main/java/com/deuvox/mapper/AuthMapper.kt
+++ data/src/main/java/com/deuvox/mapper/AuthMapper.kt
@@ -1,8 +1,19 @@
 package com.deuvox.mapper
 
+import com.deuvox.auth.remote.request.RegisterRequest
 import com.deuvox.auth.remote.response.AuthResponse
 import com.deuvox.domain.auth.model.Auth
 import com.deuvox.domain.auth.model.Token
+import com.deuvox.domain.auth.register.RegisterParam
+
+fun RegisterParam.toRequestBody(): RegisterRequest {
+    return RegisterRequest(
+        email = this.email,
+        fullName = this.fullName,
+        password = this.password,
+        confirmPassword = this.confirmPassword
+    )
+}
 
 fun AuthResponse?.toDomainModel(): Auth {
     return Auth(
