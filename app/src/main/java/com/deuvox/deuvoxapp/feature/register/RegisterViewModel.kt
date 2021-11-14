669d4dd66a4ac7a3fd837473775d7a25575653f9
Ichvandi <ichvandi.maulana@stockbit.com>
2021-11-14 11:47:08 +0700

    Change use case call from buildUseCase to execute

--- app/src/main/java/com/deuvox/deuvoxapp/feature/register/RegisterViewModel.kt
+++ app/src/main/java/com/deuvox/deuvoxapp/feature/register/RegisterViewModel.kt
@@ -3,7 +3,6 @@ package com.deuvox.deuvoxapp.feature.register
 import com.deuvox.deuvoxapp.base.viewmodel.BaseViewModel
 import com.deuvox.domain.auth.register.RegisterParam
 import com.deuvox.domain.auth.register.RegisterUseCase
-import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import timber.log.Timber
 import javax.inject.Inject
 
@@ -18,11 +17,9 @@ class RegisterViewModel @Inject constructor(
             "jh0nD03",
             "jh0nD03"
         )
-        registerUseCase.buildUseCase(param)
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe {
-                Timber.e("$it")
-            }
+        registerUseCase.execute(param, {
+            Timber.e("$it")
+        })
     }
 
     override fun onDispose() {
