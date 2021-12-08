b71ded07ea24a9cd4648d21c059c852ad12252c3
onirutlA <alturino001@gmail.com>
2021-12-08 21:58:04 +0700

    add: inject in LoginComponent.kt

--- app/src/main/java/com/deuvox/deuvoxapp/feature/login/di/LoginComponent.kt
+++ app/src/main/java/com/deuvox/deuvoxapp/feature/login/di/LoginComponent.kt
@@ -2,11 +2,11 @@ package com.deuvox.deuvoxapp.feature.login.di
 
 import com.deuvox.deuvoxapp.base.anotation.scope.PerActivity
 import com.deuvox.deuvoxapp.di.component.AppComponent
+import com.deuvox.deuvoxapp.feature.login.LoginActivity
 import dagger.Component
 
 @Component(dependencies = [AppComponent::class], modules = [LoginModule::class])
 @PerActivity
 interface LoginComponent {
-    // TODO (Ricky): DRP-007 - Inject Activity
-    // fun inject(loginActivity: LoginActivity)
+    fun inject(loginActivity: LoginActivity)
 }
